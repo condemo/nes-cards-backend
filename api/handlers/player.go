da63bdcf6e1de533eee1c6a12dc083ba41a4fc7b
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-02-21 22:41:26 +0100

    feat: `createPlayer` endpoint

--- api/handlers/player.go
+++ api/handlers/player.go
@@ -1,11 +1,15 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/condemo/nes-cards-backend/config"
 	"github.com/condemo/nes-cards-backend/store"
+	"github.com/condemo/nes-cards-backend/types"
 )
 
 type PlayerHandler struct {
@@ -45,6 +49,31 @@ func (h *PlayerHandler) getPlayerList(w http.ResponseWriter, r *http.Request) er
 }
 
 func (h *PlayerHandler) createPlayer(w http.ResponseWriter, r *http.Request) error {
-	// TODO:
-	return nil
+	p := new(types.Player)
+	json.NewDecoder(r.Body).Decode(p)
+
+	if p.Name == "" {
+		return NewApiError(
+			errors.New("player name is empty"),
+			"Player name is empty",
+			http.StatusBadRequest)
+	}
+
+	if ok := h.store.CheckPlayer(p.Name); ok {
+		return NewApiError(
+			errors.New("player already exists"),
+			fmt.Sprintf("%s already exists", p.Name),
+			http.StatusConflict,
+		)
+	}
+
+	if err := p.Validate(); err != nil {
+		return NewApiError(err, "invalid player name", http.StatusBadRequest)
+	}
+
+	if err := h.store.CreatePlayer(p); err != nil {
+		return err
+	}
+
+	return SendJSON(w, http.StatusCreated, p)
 }
