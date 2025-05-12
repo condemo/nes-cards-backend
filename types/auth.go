ca94b2f8655c488e0771d04ac175fba037963633
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-05-12 22:59:20 +0200

    fix: passwords with strict validation

--- types/auth.go
+++ types/auth.go
@@ -7,7 +7,7 @@ type User struct {
 
 	ID       int64  `bun:",pk,autoincrement" json:"id"`
 	Username string `bun:",notnull" json:"username" validate:"required,min=3,max=12,alphanum"`
-	Password string `bun:",notnull" json:"-" validate:"required,min=6,alphanum"`
+	Password string `bun:",notnull" json:"-" validate:"required,min=6"`
 }
 
 func (u *User) Validate() error {
