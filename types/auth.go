5c0e9c5f26cbc96730cae191d58b9a6e3665ac97
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-05-13 15:59:15 +0200

    tweak: changed `Username` validation

--- types/auth.go
+++ types/auth.go
@@ -6,7 +6,7 @@ type User struct {
 	bun.BaseModel `bun:"table:users,alias:u"`
 
 	ID       int64  `bun:",pk,autoincrement" json:"id"`
-	Username string `bun:",notnull" json:"username" validate:"required,min=3,max=12,alphanum"`
+	Username string `bun:",notnull" json:"username" validate:"required,min=6,max=16,alphanum"`
 	Password string `bun:",notnull" json:"-" validate:"required,min=6"`
 }
 
