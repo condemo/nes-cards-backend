309c655e3ced4ac2b0a3a4830d51b7db6132588e
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-05-13 22:44:52 +0200

    feat: `RequireAuth` error check and validation

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -1,24 +1,53 @@
 package middlewares
 
 import (
-	"encoding/json"
+	"context"
+	"errors"
 	"net/http"
 	"strings"
+
+	"github.com/condemo/nes-cards-backend/api/handlers"
+	"github.com/condemo/nes-cards-backend/api/utils"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func RequireAuth(next http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return handlers.MakeHandler(func(w http.ResponseWriter, r *http.Request) error {
 		rawToken := r.Header.Get("Authorization")
 		splitT := strings.Split(rawToken, "Bearer ")
 		token := splitT[1]
 
 		// TODO: JWT validation, etc
 		if token == "" {
-			json.NewEncoder(w).Encode(map[string]any{
-				"msg":    "invalid authorization",
-				"status": http.StatusBadRequest,
-			})
-			return
+			return handlers.ApiError{
+				Err:    errors.New("empty token"),
+				Msg:    "authorization header not found",
+				Status: http.StatusUnauthorized,
+			}
 		}
-	}
+
+		claims, err := utils.ValidateJWT(token)
+		if err != nil {
+			if errors.Is(err, jwt.ErrTokenExpired) {
+				return handlers.ApiError{
+					Err:    err,
+					Msg:    "access_token, is expired",
+					Status: http.StatusGone,
+				}
+			}
+			if errors.Is(err, jwt.ErrTokenMalformed) {
+				return handlers.ApiError{
+					Err:    err,
+					Msg:    "invalid token format",
+					Status: http.StatusUnauthorized,
+				}
+			}
+			return err
+		}
+
+		ctx := context.WithValue(r.Context(), "userID", claims.UserID)
+		next.ServeHTTP(w, r.Clone(ctx))
+
+		return nil
+	})
 }
